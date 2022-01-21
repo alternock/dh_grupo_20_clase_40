97693f82ce2215894eab6a6db8a5122f07a45632
alternock <montilla269@gmail.com>
2022-01-20 22:03:59 -0300

    localstore

---
+++ js/store.js
@@ -0,0 +1,18 @@
+const button = document.querySelector("button");
+const h2 = document.querySelector("h2");
+let userInfo;
+
+
+localStorage.setItem("avatar", "monkey nft");
+
+// localStorage.getItem()
+// localStorage.removeItem();
+
+
+
+
+// button.addEventListener("click", function () {
+//   if() {
+//      localStorage.setItem("user", JSON.stringify(userInfo))
+//   }
+// })
